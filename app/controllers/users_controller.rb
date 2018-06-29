e04ad1045dda9a2e7732055c101d2099de0f7ae7
Raita Kudo <qdrait@gmail.com>
2018-06-29 07:46:12 +0000

    会員登録後にログイン画面に遷移するよう修正

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params) 
         if @user.save
-            redirect_to blog_path
+            redirect_to blogs_path
         else
             render 'new'
         end
