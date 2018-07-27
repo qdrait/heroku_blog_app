ed8849303f88128d1cbe164597be3278b229da48
Raita Kudo <qdrait@gmail.com>
2018-07-27 10:39:37 +0000

    送信先を投稿者に変更

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,6 +1,7 @@
 class ContactMailer < ApplicationMailer
     def contact_mail(blog)
          @blog = blog
-         mail to: "qdrait@gmail.com", subject: @blog.user.email
+        #  mail to: "qdrait@gmail.com", subject: @blog.user.email
+         mail to: @blog.user.email, subject: "ブログ投稿完了通知"
     end
 end
